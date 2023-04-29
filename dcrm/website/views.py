8644ddd34235849180f263aaadaa962989ae1e00
Erick Vivas <erickvivas415@gmail.com>
2023-04-28 18:53:41 -0700

    Models for the CRM

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm 
 
 # Create your views here.
 
@@ -28,6 +29,20 @@ def logout_user(request):
     return redirect('home')
 
 def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticated 
+            username = form.cleaned_data['user_name']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You have successfully Registered")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form': form})
     
-    return render(request, 'register.html', {})
+    return render(request, 'register.html', {'form': form})
 
